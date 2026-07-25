b63f96c5163b3ca640ebcbc74d8a0995e730099a
Nithin Sanjay <nithinsanjay5@gmail.com>
2026-07-25 19:52:39 +0530

    changes

--- medical_voice_ai/android/build.gradle.kts
+++ medical_voice_ai/android/build.gradle.kts
@@ -1,51 +1,20 @@
-import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.LibraryExtension
-
-buildscript {
-    val kotlinVersion: String by extra("2.2.20")
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://www.jitpack.io")
     }
 }
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin" &&
-                requested.name.startsWith("kotlin")) {
-                useVersion("2.2.20")
-            }
-        }
-    }
-
-    plugins.withId("com.android.application") {
-        extensions.configure<BaseExtension>("android") {
-            ndkVersion = "28.2.13676358"
-        }
-    }
-
-    plugins.withId("com.android.library") {
-        extensions.configure<LibraryExtension>("android") {
-            ndkVersion = "28.2.13676358"
-        }
-    }
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
-extra["kotlin.version"] = "2.2.20"
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
